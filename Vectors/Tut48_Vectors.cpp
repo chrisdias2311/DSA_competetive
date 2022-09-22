eb735e24d8cc2fbf405139ca9d2d12f5a3bd3875
Chris Dias <105737889+chrisdias2311@users.noreply.github.com>
2022-09-22 19:03:25 +0530

    Vectors

---
+++ Vectors/Tut48_Vectors.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+// int main(){
+
+//     vector<int> v;
+//     v.push_back(1);
+//     v.push_back(2);
+//     v.push_back(3);
+
+//     for(int i=0; i<v.size(); i++){
+//         cout<<v[i]<<" ";
+//     }
+
+//     vector<int>::iterator it;
+//     for(it=v.begin() ; it!=v.end() ; it++){
+//         cout<<*it<<" ";
+//     }
+
+//     for(auto i:v){
+//         cout<<i<<" ";
+//     }
+
+//     v.pop_back();
+
+//     vector<int> v2 = (3,50);     //(size, element)  v2 = 50 50 50
+
+//     swap(v,v2);
+
+
+//     sort(b.begin(), v.end());
+
+//     return 0;
+// }
